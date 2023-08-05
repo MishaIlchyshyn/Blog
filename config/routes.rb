9a3b57e76e81b443a677a946432c7965464cdd45
Misha Ilchyshyn <ilchyshynmuchailo@gmail.com>
2023-08-05 12:42:18 +0300

    Add CategoriesController and functional tests for it

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :users, except: [:new]
   resources :articles
+  resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 end
